f8ca005130b5514c9e24812ca902197577b866e7
vigneshjraju <vigneshjraju@gmail.com>
2025-05-26 11:52:27 +0530

    ok

--- api/[id].js
+++ api/[id].js
@@ -2,5 +2,5 @@ const MetaData=require('../Metadata.json');
 
 Module.exports=(req,res)=>{
     const {id}=req.query;
-    res.json(JSON.parse(JSON.stringify(Metadata[id])));
+    res.json(JSON.parse(JSON.stringify(MetaData[id])));
 } 
